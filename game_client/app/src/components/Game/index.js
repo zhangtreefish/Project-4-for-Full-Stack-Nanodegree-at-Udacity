87808682f2e6ab2bc6b3637bdd625975f7cccc7f
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-11-28 12:12:23 -0600

    Fix: display game data

--- game_client/app/src/components/Game/index.js
+++ game_client/app/src/components/Game/index.js
@@ -2,14 +2,24 @@ import React, { PropTypes } from 'react';
 
 import styles from './index.module.scss';
 import cssModules from 'react-css-modules';
+import Box from 'grommet-udacity/components/Box';
+import Paragraph from 'grommet-udacity/components/Paragraph';
 
-const Game = (props) => (
-  <div className={styles.game}>
-  </div>
+const Game = ({
+  game,
+}) => (
+  <Box>
+    <Paragraph> name:{game.name} </Paragraph>
+    <Paragraph> seatsAvailable:{game.seatsAvailable} </Paragraph>
+    <Paragraph> playerOne:{game.playerOne} </Paragraph>
+    <Paragraph> playerTwo:{game.playerTwo} </Paragraph>
+    <Paragraph> gameCurrentMove:{game.gameCurrentMove} </Paragraph>
+    <Paragraph> gameBoard:{game.gameBoard} </Paragraph>
+  </Box>
 );
 
 Game.propTypes = {
-
+  game: PropTypes.object.isRequired,
 };
 
 export default cssModules(Game, styles);
